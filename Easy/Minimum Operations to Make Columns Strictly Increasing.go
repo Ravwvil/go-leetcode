1da2a1c394d7ac247cc00dfefad11c3677551cca
Ravwvil <rawil.kazeev@gmail.com>
2025-02-13 15:50:23 +0300

    start

---
+++ Easy/Minimum Operations to Make Columns Strictly Increasing.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	grid := [][]int{}
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+		rowStrs := strings.Fields(line)
+		row := make([]int, len(rowStrs))
+		for j, numStr := range rowStrs {
+			row[j], _ = strconv.Atoi(numStr)
+		}
+		grid = append(grid, row)
+	}
+	println(minimumOperations(grid))
+}
+
+func minimumOperations(grid [][]int) int {
+	counter := 0
+
+	for j := 0; j < len(grid[0]); j++ {
+		for i := 1; i < len(grid); i++ {
+			if grid[i][j] <= grid[i-1][j] {
+				counter += grid[i-1][j] - grid[i][j] + 1
+				grid[i][j] = grid[i-1][j] + 1
+			}
+		}
+	}
+	return counter
+}
