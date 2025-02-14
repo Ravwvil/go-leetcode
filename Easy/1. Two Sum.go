a9e9781d6d24c7b6c558f13e10ac9a7ba2d2699a
Ravwvil <rawil.kazeev@gmail.com>
2025-02-14 14:43:00 +0300

    refactor one of previous task

---
+++ Easy/1. Two Sum.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var nums []int
+	var target int
+
+	reader := bufio.NewReader(os.Stdin)
+
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+
+	for _, s := range strings.Fields(input) {
+		num, _ := strconv.Atoi(s)
+		nums = append(nums, num)
+	}
+
+	fmt.Scan(&target)
+
+	fmt.Println(twoSum(nums, target))
+}
+
+func twoSum(nums []int, target int) []int {
+	pairs := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		if _, ok := pairs[nums[i]]; ok {
+			return []int{pairs[nums[i]], i}
+		}
+		pairs[target-nums[i]] = i
+	}
+	return []int{0, 0}
+}
