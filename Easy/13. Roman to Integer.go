43315f65df166dd7406ae3567898ebc7362d7ae4
Ravwvil <rawil.kazeev@gmail.com>
2025-02-28 12:32:09 +0300

    add task

--- Easy/13. Roman to Integer.go
+++ Easy/13. Roman to Integer.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	s := scanner.Text()
+	fmt.Println(romanToInt(s))
+}
+
+func romanToInt(s string) int {
+	symbols := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	result := 0
+	for i := 0; i < len(s); i++ {
+		if i+1 < len(s) && symbols[s[i]] < symbols[s[i+1]] {
+			result -= symbols[s[i]]
+		} else {
+			result += symbols[s[i]]
+		}
+	}
+	return result
+}
--- Easy/20. Valid Parentheses.go
+++ Easy/20. Valid Parentheses.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	s := scanner.Text()
+	fmt.Println(isValid(s))
+}
+
+func isValid(s string) bool {
+	stack := make([]rune, 0)
+
+	for _, r := range s {
+		if r == '(' || r == '[' || r == '{' {
+			stack = append(stack, r)
+		} else {
+			if len(stack) == 0 {
+				return false
+			}
+
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			if (r == ')' && top != '(') || (r == ']' && top != '[') || (r == '}' && top != '{') {
+				return false
+			}
+		}
+	}
+
+	return len(stack) == 0
+}
