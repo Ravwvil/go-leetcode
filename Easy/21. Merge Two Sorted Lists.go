c5fd8f676af6b618fccc3560affe73bf5421fa6d
Ravwvil <rawil.kazeev@gmail.com>
2025-05-24 00:41:11 +0300

    Add one more solution for task 21

--- Easy/21. Merge Two Sorted Lists.go
+++ Easy/21. Merge Two Sorted Lists.go
@@ -19,18 +19,41 @@ func main() {
 	}
 }
 
-func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	if l1 == nil {
-		return l2
-	}
-	if l2 == nil {
-		return l1
-	}
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	op := dummy
 
-	if l1.Val < l2.Val {
-		l1.Next = mergeTwoLists(l1.Next, l2)
-		return l1
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			op.Next = list1
+			list1 = list1.Next
+		} else {
+			op.Next = list2
+			list2 = list2.Next
+		}
+		op = op.Next
 	}
-	l2.Next = mergeTwoLists(l1, l2.Next)
-	return l2
+	if list1 != nil {
+		op.Next = list1
+	}
+	if list2 != nil {
+		op.Next = list2
+	}
+	return dummy.Next
 }
+
+//func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+//	if l1 == nil {
+//		return l2
+//	}
+//	if l2 == nil {
+//		return l1
+//	}
+//
+//	if l1.Val < l2.Val {
+//		l1.Next = mergeTwoLists(l1.Next, l2)
+//		return l1
+//	}
+//	l2.Next = mergeTwoLists(l1, l2.Next)
+//	return l2
+//}
